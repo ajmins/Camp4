1db0534d6e7729f31f42f96b32b540954091d8c5
Ajmi <>
2022-08-17 21:45:30 +0530

    assignmet done

--- assignments/clientphone.py
+++ assignments/clientphone.py
@@ -1,62 +1,74 @@
-import socket
 import json
+import socket
+import re
+phoneNumbers = {}
+class Utils:
+    def getInt(*msg):
+        while(True):
+            try:
+                value = int(input(*msg))
+                return value
+            except:
+                print("Enter a valid number")
+                continue
 
-phoneBook = dict()
+    def getNumber(*msg):
+        while(True):
+                value = input(*msg)
+                if re.match(r'[0-9]*', value):
+                    return value
+                else:
+                    print("Enter a valid number")
+                    continue
 
-#to list contacts
+
 def list():
-    phoneDetails = {"ch":1, "args":None}
+    msgJson = {"ch" : 1, "args":None}
+    return msgJson
 
-#to add a new contact
 def add():
-    details = dict()
-    name = input("Enter the name of new contact: ")
-    num = input("Enter the phone number: ")
-    phoneDetails = {"ch":2, "args":{"name":name,"num":num}}
-    return phoneDetails
+    msg = {}
+    name = input("Enter name: ")
+    phoneNumber = Utils.getNumber("Enter Phone Number: ")
+    msgJson = {"ch":2, "args":{"name":name,"phn":phoneNumber}}
+    return msgJson
 
-#to remove a contact
+
 def delete():
-    name = input("Enter the name of contact to delete: ")
-    phoneDetails = {"ch":3, "args":{"name":name}}
-    return phoneDetails
+    
+    nameToBeDeleted = input("Enter the name to be deleted: ")
+    msgJson = {"ch":3, "args":{"name":nameToBeDeleted}}
+    return msgJson
 
-#to search a contact by name
-def searchName():
-    name = input("Enter the name of contact to search: ")
-    phoneDetails = {"ch":4, "args":{"name":name}}
-    return phoneDetails
+def searchByName():
+    name = input("Enter the name to be searched: ")
+    msgJson = {"ch":4, "args":{"name":name}}
+    return msgJson
 
-#to search a contact by number
-def searchNo():
-    num = int((input("Enter the phone number to search: ")))
-    phoneDetails = {"ch":5, "args":{"num":num}}
-    return phoneDetails
+def searchbyNumber():
+    phn = Utils.getNumber("Enter the phone number: ")
+    msgJson = {"ch":5, "args":{"phn":phn}}
+    return msgJson
 
-def get_userDetails(ch):
-    global details
-    details =""
-    if ch == 1:
-        details = list()
-    elif ch == 2:
-        details = add()
-    elif ch == 3:
-        details = delete()
-    elif ch == 4:
-        details = searchName()
-    elif ch == 5:
-        details = searchNo()
-    elif ch == 6:
-        exit()
-    else:
-        print("Wrong choice")
-    return json.dumps(details)
 
-def handleReply(detailsString):
-    details = json.loads(detailsString)
-    print(details['status'])
-    if (details['value']):
-        print(details['value'])
+def actionToSelectedOption(ch):
+    global msg
+    msg = ""
+    match(ch):
+        case 1: msg = list()
+        case 2: msg = add()
+        case 3: msg = delete()
+        case 4: msg = searchByName()
+        case 5: msg = searchbyNumber()
+        case 6: msg = exit()
+        case _: print("Wrong choice")
+    return json.dumps(msg)
+            
+def handleReply(msgString):
+    msg = json.loads(msgString)
+    print(msg['status'])
+    if (msg['value']):
+        print(msg['value'])
 
 def client_program():
     host = socket.gethostname() #get the hostname
@@ -71,19 +83,20 @@ def client_program():
     #instead of binding, client we are connecting to server
     client_socket.connect((host,port)) #host & port as tuple
     
+    #getting the msg to send to the server
 
-    while(True):
-        ch =int(input("""
-    1.List all contacts
-    2.Add a new contact
-    3.Delete a contact
-    4.Search by name
-    5.Search by number
-    6.Exit & Disconnect
-
-    Enter the choice: 
-    """))
-        message = get_userDetails(ch)
+    while True:
+        ch = Utils.getInt("""
+    Enter the choice:
+    1) List all contacts
+    2) Add a new contact
+    3) Delete a contact
+    4) Search by name
+    5) Search by number
+    6) exit
+    
+    """)
+        message = actionToSelectedOption(ch)
         if ch == 6:
             break
         #if the msg is not 'exit', encode and send it to server
@@ -94,9 +107,11 @@ def client_program():
         handleReply(data)
         #getting the msg to send to the server
         
-        
+
     #close the socket connection once the while loop is exited
+    print("closing connection ...")
     client_socket.close()
 
 if __name__ == '__main__': 
     client_program()   
+
--- assignments/serverphone.py
+++ assignments/serverphone.py
@@ -1,114 +1,97 @@
 import socket
-import re
 import json
-
-phoneBook = dict()
+phoneNumbers = {}
 
 def add(args):
     name = args['name']
-    num = args['num']
-    phoneBook[name] = num 
-    print("Contact added")
-    details = json.dumps({"status" : "Contact added successfully",
+    phoneNumber = args['phn']
+    phoneNumbers[name] = phoneNumber 
+    msg = json.dumps({"status" : "Contact added successfully",
     "value" :None})
-    return details
+    return msg
+    
 
 def list():
     value = ""
-    for i,x in enumerate(phoneBook):
-        value += "\n{}. Name: {} Phonenumber: {}".format(i+1,x,phoneBook[x])
-    details = json.dumps({"status" : "Found Contact",
+    for i,x in enumerate(phoneNumbers):
+        value += "\n{}. Name: {}\nPhonenumber: {}".format(i+1,x,phoneNumbers[x])
+    msg = json.dumps({"status" : "Found Contacts",
     "value" :value})
-    return details
-
-def delete(args):
+    return msg
     
-    name = args['name']
-    if name in phoneBook.keys():
-        del phoneBook[name]
+def delete(args):
+    nameToBeDeleted = args['name']
+    if nameToBeDeleted in phoneNumbers.keys():
+        del phoneNumbers[nameToBeDeleted]
         status = "Number Deleted!"
     else: 
         status = "Name not found"
-    details = json.dumps({"status" : status,
+    msg = json.dumps({"status" : status,
     "value" :None})
-    return details
+    return msg
 
-def searchName(args):
+def searchByName(args):
     name = args['name']
-    if name in phoneBook.keys():
+    if name in phoneNumbers.keys():
         status = "Name found"
-        value = "Name: {}     Phone Number: {}".format(name,phoneBook[name])
+        value = "Name: {}\nPhone Number: {}".format(name,phoneNumbers[name])
     else:
         status = "Name not found"
-    details = json.dumps({"status" : status,
+        value = None
+    msg = json.dumps({"status" : status,
     "value" :value})
-    return details
+    return msg
 
-def searchNo(args):
-    num = args['phn']
-    if num in phoneBook.values():
-        for name,phn_number in phoneBook.items():
-            if phn_number == num:
+
+def searchbyNumber(args):
+    phn = args['phn']
+    if phn in phoneNumbers.values():
+        for name,phn_number in phoneNumbers.items():
+            if phn_number == phn:
                 status = "Name found"
-                value = "Name : {} Phone Number: {}".format(name,phn_number)
+                value = "Name : {}\nPhone Number: {}".format(name,phn_number)
     else:
         status = "Phone number not found"
-    details = json.dumps({"status" : status,
+        value = None
+    msg = json.dumps({"status" : status,
     "value" :value})
-    return details
+    return msg
 
-def get_userDetails(ch, args):
-    global details
-    details =""
-    if ch == 1:
-        details = list()
-    elif ch == 2:
-        details = add(args)
-    elif ch == 3:
-        details = delete(args)
-    elif ch == 4:
-        details = searchName(args)
-    elif ch == 5:
-        details = searchNo(args)
-    elif ch == 6:
-        exit()
-    else:
-        print("Wrong choice")
-    return details
+def actionToSelectedOption(ch, args):
+    global msg
+    msg = ""
+    match(ch):
+        case 1: msg = list()
+        case 2: msg =  add(args)
+        case 3: msg = delete(args)
+        case 4: msg = searchByName(args)
+        case 5: msg = searchbyNumber(args)
+        case 6: msg = exit()
+        case _: msg =print("Wrong choice")
+    return msg   
 
-def handleRequest(jsonString):
+def handlerequest(jsonString):
     message = json.loads(jsonString)
-    details = get_userDetails(message["ch"],message["args"],)
-    return details
+    msg = actionToSelectedOption(message["ch"],message["args"],)
+    return msg
 
-
+    
 def server_program():
-    host = socket.gethostname() #get the hostname
-    port = 5000 #initiate port no above 1024 till 65535
-
-    server_socket = socket.socket() #get instance of socket
-
+    host = socket.gethostname() # getting the ip of host
+    port = 5000
+    server_socket = socket.socket()
     server_socket.bind((host,port))
-
     server_socket.listen(2)
-    #the accept method will give back the connection object and ip address of the incoming connection request
-    conn, address = server_socket.accept() #accept new connection
-    print("Connection from: " + str(address))
-
+    conn, address = server_socket.accept()
+    print("Connection accepted from " + str(address))
     while True:
-        #infinite while loop to receive data stream
-        #it won't accept the data packet greater than 1024 bytes
-        #decode the received data
         data = conn.recv(1024).decode()
         if not data:
-            #if data is not received, then break the while loop
             break
-            #if valid data we can print the data received
-        print("Message from client " + str(address) + " : " + str(data))
-        #give provision to send reply back to the clinet
-        details = handleRequest(str(data))
-        conn.send(details.encode()) #encode the data and send data to the client    
-    conn.close() #close the connection once the while loop breaks
+        print("Message from client" + str(address) + " -> " + str(data))
+        msg = handlerequest(str(data))
+        conn.send(msg.encode())
+    conn.close()
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     server_program()
